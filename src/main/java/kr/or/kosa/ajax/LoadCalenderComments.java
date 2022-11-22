cd98b4bf87970f0e0ae797911e74d6fa36b9f352
park71403 <park71403@gmail.com>
2022-11-22 11:29:48 +0900

    캘린더 댓글 목록 불러오기 기능 추가

---
+++ src/main/java/kr/or/kosa/ajax/LoadCalenderComments.java
@@ -0,0 +1,57 @@
+package kr.or.kosa.ajax;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.or.kosa.dao.Calender_Dao;
+import kr.or.kosa.dao.Comments_Dao;
+import kr.or.kosa.dto.Comments;
+import net.sf.json.JSONArray;
+
+@WebServlet("/LoadCalenderComments")
+public class LoadCalenderComments extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoadCalenderComments() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    private void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+    	request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+    	PrintWriter out = response.getWriter();
+    	
+    	try {  		
+			int idx = Integer.parseInt(request.getParameter("idx"));
+			
+			Comments_Dao dao = new Comments_Dao();
+			List<Comments> colist = dao.getCommentListByIdx(idx);
+			
+			JSONArray jsonlist = JSONArray.fromObject(colist);
+			
+			out.print(jsonlist);
+    		
+    	} catch(Exception e) {
+    		System.out.println(e.getMessage());
+    	}
+    	
+	}
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+}
